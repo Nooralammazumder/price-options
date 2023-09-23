59c0a5ce64c716d2f532222aa8b38775780cba80
Nooralammazumder <125288646+Nooralammazumder@users.noreply.github.com>
2023-09-23 19:03:25 +0600

    navbar done for this project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+// import DaisyNav from "./Components/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
+      <h1 className="text-7xl bg-yellow-400">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
